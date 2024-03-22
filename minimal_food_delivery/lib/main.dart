84d01cd7406dd02c909bd8e6c2f445d985ad42af
Sagar <Sagarchawan4646@gmail.com>
2024-03-23 02:43:14 +0530

    MFD: The Data setup

--- minimal_food_delivery/lib/main.dart
+++ minimal_food_delivery/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:minimal_food_delivery/pages/login_page.dart';
+import 'package:minimal_food_delivery/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +19,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
       home: const LoginPage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
--- minimal_food_delivery/lib/pages/login_page.dart
+++ minimal_food_delivery/lib/pages/login_page.dart
@@ -7,6 +7,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
         title: const Text("Login Page"),
       ),
     );
