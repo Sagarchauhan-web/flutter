2910e567198364b1637cde65cd1e0b4e60842465
Sagar <Sagarchawan4646@gmail.com>
2024-03-23 01:21:12 +0530

    SimpleEcommerce: Theme changes

--- simple_ecommerce/lib/themes/light_mode.dart
+++ simple_ecommerce/lib/themes/light_mode.dart
@@ -6,3 +6,10 @@ ThemeData lightMode = ThemeData(
         primary: Colors.grey.shade200,
         secondary: Colors.white,
         inversePrimary: Colors.grey.shade700));
+
+// ThemeData lightMode = ThemeData(
+//     colorScheme: const ColorScheme.light(
+//         surface: Color.fromARGB(255, 199, 249, 204),
+//         primary: Color.fromARGB(255, 56, 163, 165),
+//         secondary: Color.fromARGB(255, 128, 237, 153),
+//         inversePrimary: Color.fromARGB(255, 128, 237, 153)));
