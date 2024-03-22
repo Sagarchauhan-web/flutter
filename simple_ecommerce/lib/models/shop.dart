dbdf13d851ab81cd31b3f6cdbc68f6853556bba3
Sagar <Sagarchawan4646@gmail.com>
2024-03-22 14:16:44 +0530

    SimpleEcommerce: Models

---
+++ simple_ecommerce/lib/models/shop.dart
@@ -0,0 +1,43 @@
+import 'package:simple_ecommerce/models/product.dart';
+
+class Shop {
+  // Products
+  final List<Product> _shop = [
+    Product(
+        name: "Product 1",
+        description: "This is product 1",
+        price: 100,
+        imagePath: '/'),
+    Product(
+        name: "Product 2",
+        description: "This is product 2",
+        price: 100,
+        imagePath: '/'),
+    Product(
+        name: "Product 3",
+        description: "This is product 3",
+        price: 100,
+        imagePath: '/'),
+    Product(
+        name: "Product 4",
+        description: "This is product 4",
+        price: 100,
+        imagePath: '/'),
+  ];
+
+  // user cart
+  List<Product> _cart = [];
+
+  // get product and cart
+  List<Product> get shop => _shop;
+  List<Product> get cart => _cart;
+
+  // add product
+  void addToCart(Product item) {
+    _cart.add(item);
+  }
+
+  void removeFromCart(Product item) {
+    _cart.remove(item);
+  }
+}
